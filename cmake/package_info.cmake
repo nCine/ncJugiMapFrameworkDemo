55212fdecc26fa4ce59921defad3c600a3768de7
Angelo Theodorou <encelo@gmail.com>
2020-06-25 17:41:56 +0200

    Modify package info script to support API updates
    - Update vendor and homepage information
    - Using develop branch of nCine for Azure Pipelines

--- cmake/package_info.cmake
+++ cmake/package_info.cmake
@@ -1,10 +1,10 @@
 set(PACKAGE_NAME "ncJugiMapAPIDemo")
 set(PACKAGE_EXE_NAME "ncjugimapapidemo")
-set(PACKAGE_VENDOR "Angelo Theodorou and Jugilus")
+set(PACKAGE_VENDOR "Jugilus")
 set(PACKAGE_COPYRIGHT "Copyright ©2019-2020 ${PACKAGE_VENDOR}")
 set(PACKAGE_DESCRIPTION "A nCine port of the JugiMap API Demo test by Jugilus")
-set(PACKAGE_HOMEPAGE "https://ncine.github.io")
-set(PACKAGE_REVERSE_DNS "io.github.ncine.ncjugimapapidemo")
+set(PACKAGE_HOMEPAGE "http://jugimap.com")
+set(PACKAGE_REVERSE_DNS "com.jugimap.apidemo.ncine")
 
 set(PACKAGE_INCLUDE_DIRS src/)
 
@@ -20,6 +20,8 @@ set(PACKAGE_SOURCES
 	src/engineIndependent/various.h
 	src/engineIndependent/various.cpp
 
+	src/jugimap/jmAnimationCommon.h
+	src/jugimap/jmAnimationCommon.cpp
 	src/jugimap/jmCamera.cpp
 	src/jugimap/jmCamera.h
 	src/jugimap/jmCollision.cpp
@@ -53,6 +55,10 @@ set(PACKAGE_SOURCES
 	src/jugimap/jmStreams.h
 	src/jugimap/jmText.cpp
 	src/jugimap/jmText.h
+	src/jugimap/jmTimelineAnimation.h
+	src/jugimap/jmTimelineAnimation.cpp
+	src/jugimap/jmTimelineAnimationInstance.h
+	src/jugimap/jmTimelineAnimationInstance.cpp
 	src/jugimap/jmTween.cpp
 	src/jugimap/jmTween.h
 	src/jugimap/jmUtilities.cpp
